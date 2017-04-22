be318063aabdbc311958cf93ab6023d640d5c886
Carolina Oconitrillo <Carolina Oconitrillo>
2017-04-22 11:44:22 -0600

    activity para opcion de login y registro

---
+++ app/src/main/java/com/example/caro/microadmin/Login_Registro_Activity.java
@@ -0,0 +1,13 @@
+package com.example.caro.microadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Registro_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__registro);
+    }
+}
