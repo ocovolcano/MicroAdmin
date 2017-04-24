9cd563aeb266645840e5c4882b836e725a96842e
ocovolcano <carooco30@gmail.com>
2017-04-24 10:58:37 -0600

    Create Registro.php

---
+++ Registro.php
@@ -0,0 +1,15 @@
+<?php
+    $con = mysqli_connect("mysql10.000webhost.com", "id1453347_microadmin", "microadmin17", "id1453347_microadmin");
+    
+    $Nombre = $_POST["nombre"];
+    $Correo = $_POST["correo"];
+    $Contrasena = $_POST["contrasena"];
+    $statement = mysqli_prepare($con, "INSERT INTO user (Nombre, Correo, Contrasena) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $Nombre, $Correo, $Contrasena);
+    mysqli_stmt_execute($statement);
+    
+    $response = array();
+    $response["success"] = true;  
+    
+    echo json_encode($response);
+?>
