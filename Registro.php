cb23a5375df65d419bf762e3b9b97e692ca070a7
ocovolcano <carooco30@gmail.com>
2017-04-24 11:04:56 -0600

    Update Registro.php

--- Registro.php
+++ Registro.php
@@ -4,7 +4,7 @@
     $Nombre = $_POST["nombre"];
     $Correo = $_POST["correo"];
     $Contrasena = $_POST["contrasena"];
-    $statement = mysqli_prepare($con, "INSERT INTO user (Nombre, Correo, Contrasena) VALUES (?, ?, ?, ?)");
+    $statement = mysqli_prepare($con, "INSERT INTO user (Nombre, Correo, Contrasena) VALUES (?, ?, ?)");
     mysqli_stmt_bind_param($statement, "siss", $Nombre, $Correo, $Contrasena);
     mysqli_stmt_execute($statement);
     
