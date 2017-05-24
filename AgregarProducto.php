c3cb9d27a6b2a5bcb587ef0802fc402e2f434d5f
ocovolcano <carooco30@gmail.com>
2017-05-24 10:03:13 -0600

    Add files via upload

---
+++ AgregarProducto.php
@@ -0,0 +1,68 @@
+<?php
+    $con = mysqli_connect("localhost", "id1453347_microadmin", "microadmin17", "id1453347_microadmin");
+    
+    $codigo = $_POST["codigo"];
+    $nombre = $_POST["nombre"];
+    $preciounidad = $_POST["preciounidad"];
+    $costomanufactura = $_POST["costomanufactura"];
+    $imagen = $_FILES["imagen"];
+    $cantidad = $_POST["cantidad"];
+    $idproducto;
+    $uploadPath = 'img/';
+    //$serverIP = gethostbyname(gethostname());
+    $uploadURL = 'http://microadmin.000webhostapp.com/'.$uploadPath;
+    //agregar lo del url a imagen
+
+    function ingresarProducto() {
+    global $con, $codigo, $nombre, $preciounidad, $costomanufactura, $idproducto, $imagen, $uploadURL, $uploadPath;
+
+    $fileInfo = pathinfo($imagen);
+    $extension = $fileInfo['extension'];
+    $fileURL = $uploadURL. getFileName() . '.' . $extension;
+    $filePath = $uploadPath. getFileName() . '.' . $extension;
+
+
+    try{
+        move_uploaded_file($imagen, $uploadPath);
+        $statement = mysqli_prepare($con, "INSERT INTO Producto (Codigo, Nombre, PrecioUnidad, CostoManufacturaUnidad, URLImagen) VALUES (?, ?, ?, ?, ?)");
+        mysqli_stmt_bind_param($statement,'ssdds', $codigo, $nombre, $preciounidad, $costomanufactura, $fileURL);
+        $result = mysqli_stmt_execute($statement);
+
+        if ($result === TRUE) {
+            $idproducto = mysqli_insert_id($con);
+            ingresarInventario();
+        }
+        mysqli_stmt_close($statement);  
+
+    }catch(Exception $e){
+        $response['error'] = false;
+
+     }
+    }
+
+    function getFileName(){
+        $con = mysqli_connect("localhost", "id1453347_microadmin", "microadmin17", "id1453347_microadmin");
+        $statement = "SELECT MAX(IDProducto) AS IDProducto FROM Producto";
+        $result = mysqli_fetch_array(mysqli_query($con, $statement));
+        mysqli_close($con);
+        if($result['IDProducto'] == null){
+            return 1;
+        }else{
+            return ++$result['IDProducto'];
+        }
+
+    }
+
+    function ingresarInventario(){
+        global $con, $idproducto, $codigo, $cantidad;
+        $statement = mysqli_prepare($con, "INSERT INTO Inventario (IDProducto, Cantidad) VALUES (?, ?)");
+        mysqli_stmt_bind_param($statement,'ii', $idproducto, $cantidad);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_close($statement);  
+    }
+
+
+    ingresarProducto();
+    $response = array();
+    $response["success"] = true;  
+?>
